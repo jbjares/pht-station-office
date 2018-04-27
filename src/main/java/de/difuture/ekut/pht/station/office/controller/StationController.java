e38e23846b4acf7f0d085b6714a4f55720fd22af
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-27 20:31:59 +0200

    [REFACTOR] Rename Train to APITrain

--- src/main/java/de/difuture/ekut/pht/station/office/controller/StationController.java
+++ src/main/java/de/difuture/ekut/pht/station/office/controller/StationController.java
@@ -5,8 +5,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import de.difuture.ekut.pht.lib.core.api.Station;
-import de.difuture.ekut.pht.lib.core.neo4j.entity.StationEntity;
+import de.difuture.ekut.pht.lib.core.api.APIStation;
+import de.difuture.ekut.pht.lib.core.neo4j.entity.Station;
 import de.difuture.ekut.pht.station.office.repository.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,7 +22,7 @@ import javax.validation.constraints.NotNull;
 @RequestMapping(value = "/station")
 public class StationController {
 
-	private static final ResponseEntity<Station> NOT_FOUND
+	private static final ResponseEntity<APIStation> NOT_FOUND
 			= ResponseEntity.notFound().build();
 	
 	private final StationRepository stationRepository;
@@ -35,35 +35,38 @@ public class StationController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public Iterable<Station> ping(@RequestBody @Valid Station station) {
+	public Iterable<APIStation> ping(@RequestBody @Valid APIStation apiStation) {
 
 	    final Instant lastPing = Instant.now();
-	    final List<StationEntity> entities = this.stationRepository
-                .findAllByStationURI(station.getStationURI());
+	    final List<Station> entities = this.stationRepository
+                .findAllByStationURI(apiStation.getStationURI());
 
 	    if (entities.isEmpty()) {
 
-	        return Collections.singleton(this.stationRepository.save(new StationEntity(station, lastPing)).toStation());
+	        final Station station = new Station(apiStation, lastPing);
+	        station.setActive(true);
+	        return Collections.singleton(this.stationRepository.save(station).toStation());
 
         } else {
 
 	        return entities.stream().map(x -> {
 	            x.setLastPing(lastPing);
+	            x.setActive(true);
 	            return this.stationRepository.save(x).toStation();
             }).collect(Collectors.toList());
         }
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public Iterable<Station> getAll() {
+	public Iterable<APIStation> getAll() {
 
 		return StreamSupport.stream(this.stationRepository.findAll().spliterator(), false)
-                .map(StationEntity::toStation)
+                .map(Station::toStation)
                 .collect(Collectors.toList());
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Station> getOne(
+	public ResponseEntity<APIStation> getOne(
 			@PathVariable("id") UUID trainID) {
 
 		return this.stationRepository
--- src/main/java/de/difuture/ekut/pht/station/office/repository/StationRepository.java
+++ src/main/java/de/difuture/ekut/pht/station/office/repository/StationRepository.java
@@ -1,6 +1,6 @@
 package de.difuture.ekut.pht.station.office.repository;
 
-import de.difuture.ekut.pht.lib.core.neo4j.entity.StationEntity;
+import de.difuture.ekut.pht.lib.core.neo4j.entity.Station;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import java.net.URI;
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-public interface StationRepository extends Neo4jRepository<StationEntity, UUID> {
+public interface StationRepository extends Neo4jRepository<Station, UUID> {
 
-    List<StationEntity> findAllByStationURI(URI stationURI);
+    List<Station> findAllByStationURI(URI stationURI);
 }
